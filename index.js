0b1e0c93c805225380f81f5e1ede39a59664ff4b
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-18 23:52:13 +0600

    added get oparetion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app= express()
 const port = process.env.PORT || 5000
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvg3x0p.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,6 +26,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCarCollection = client.db('toyCarHubDB').collection('toyCar')
+    const categoryCollection = client.db('toyCarHubDB').collection('toyCategory')
+
+    app.get('/allToys', async(req, res)=>{
+      const result = await toyCarCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/categoryToy', async(req, res)=>{
+      const result = await categoryCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/allToys/:id', async(req, res)=>{
+      const id = req.params.id
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCarCollection.findOne(query)
+      res.send(result)
+    })
+
+app.post('/addToys', async(req, res)=>{
+  const body = req.body
+  console.log(body)
+  const result = await toyCarCollection.insertOne(body)
+  res.send(result)
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,6 +70,6 @@ app.get('/', (req, res)=>{
         res.send('toy car server is running')
 })
 
-app.listen(cors, ()=>{
+app.listen(port, ()=>{
         console.log(`toy car server is running on port ${port}`)
 })
