2b1b15e36fa6a7f54bb89dad55b84aff43dde3a1
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-26 11:07:51 +0600

    added all

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
 
     const toyCarCollection = client.db('toyCarHubDB').collection('toyCar')
     const categoryCollection = client.db('toyCarHubDB').collection('toyCategory')
